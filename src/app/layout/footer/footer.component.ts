50cfebe30c049bb962815a7344477a5a11235e22
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-09-02 17:01:59 +0200

    Add new styles to navbar

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  goBackUp() {
+    window.scrollTo(0, 0);
+  }
 }

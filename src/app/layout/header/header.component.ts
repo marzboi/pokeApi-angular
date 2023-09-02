89e0b016197359e1f5db91c57ec685a6f7cdd4a2
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-09-02 13:29:29 +0200

    Add global styles

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private zone: NgZone, private router: Router) {}
 
+  handleNavigateHome() {
+    this.zone.run(() => this.router.navigate(['']));
+  }
 }

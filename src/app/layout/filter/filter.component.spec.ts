20da98b56b2155dec1198985ce8081532ea28a7b
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-09-13 20:03:51 +0200

    Add testing conditions for filter component

--- src/app/layout/filter/filter.component.spec.ts
+++ src/app/layout/filter/filter.component.spec.ts
@@ -1,15 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FilterComponent } from './filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FilterComponent } from './filter.component';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
+  let pokemonServiceMock: jasmine.SpyObj<PokemonService>;
+  let routerMock: { navigate: jasmine.Spy };
 
   beforeEach(() => {
+    pokemonServiceMock = jasmine.createSpyObj('PokemonService', [
+      'resetPokemonList',
+      'getPokemonByType',
+    ]);
+    pokemonServiceMock.resetPokemonList.and.returnValue(of(void 0));
+    pokemonServiceMock.getPokemonByType.and.returnValue(of(void 0));
+
+    routerMock = {
+      navigate: jasmine.createSpy('navigate'),
+    };
+
     TestBed.configureTestingModule({
       declarations: [FilterComponent],
       imports: [
@@ -18,7 +33,12 @@ describe('FilterComponent', () => {
         FontAwesomeModule,
         HttpClientTestingModule,
       ],
+      providers: [
+        { provide: PokemonService, useValue: pokemonServiceMock },
+        { provide: Router, useValue: routerMock },
+      ],
     });
+
     fixture = TestBed.createComponent(FilterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -26,5 +46,26 @@ describe('FilterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize with a type of 0', () => {
+    expect(component.type).toBe(0);
+  });
+
+  describe('handlePokemonByType', () => {
+    it('should navigate to loading and then home when type is 0', () => {
+      component.handlePokemonByType();
+      expect(routerMock.navigate).toHaveBeenCalledWith(['loading']);
+      expect(routerMock.navigate).toHaveBeenCalledWith(['']);
+      expect(pokemonServiceMock.resetPokemonList).toHaveBeenCalled();
+    });
+
+    it('should navigate to loading and then home when type is non-zero', () => {
+      component.type = 1;
+      component.handlePokemonByType();
+      expect(routerMock.navigate).toHaveBeenCalledWith(['loading']);
+      expect(routerMock.navigate).toHaveBeenCalledWith(['']);
+      expect(pokemonServiceMock.getPokemonByType).toHaveBeenCalledWith(1);
+    });
   });
 });

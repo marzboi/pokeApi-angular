f52bdc8da5a4858e5a3485a37172e314a6a3bd17
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-09-13 20:06:39 +0200

    Add testing condition for header componenet

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { Router } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      providers: [{ provide: Router, useValue: routerSpy }],
     });
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +22,10 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate home when handleNavigateHome is called', () => {
+    component.handleNavigateHome();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['']);
   });
 });
